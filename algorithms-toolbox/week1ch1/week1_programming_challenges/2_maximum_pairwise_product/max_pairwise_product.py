2351990d083161182d053b4a5b02c241e097fa10
Wechuli <wechulipaul@yahoo.com>
2019-05-06 17:59:16 +0300

    solving maximum pairwise challenge

--- algorithms-toolbox/week1ch1/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ algorithms-toolbox/week1ch1/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -7,12 +7,12 @@ def max_pairwise_product(numbers):
     for first in range(n):
         for second in range(first + 1, n):
             max_product = max(max_product,
-                numbers[first] * numbers[second])
+                              numbers[first] * numbers[second])
 
     return max_product
 
 
 if __name__ == '__main__':
-    input_n = int(input())
-    input_numbers = [int(x) for x in input().split()]
+    input_n = input("Insert your numbers separated by commas: ")
+    input_numbers = [int(x) for x in input_n.split()]
     print(max_pairwise_product(input_numbers))
