19f474c980bb998349f07d428918f2b44b38d061
Wechuli <wechulipaul@yahoo.com>
2019-11-14 10:30:23 +0300

    someREcursive

---
+++ algorithms-step-by-step/recursion/exercises/someRecursive.js
@@ -0,0 +1,11 @@
+// Write a recursive function called someRecursive which accepts an array and a callback. The function returns true if a single value in the array returs true when passed to the callback. Otherwise it returns false
+
+const isOdd = number => number % 2 !== 0;
+
+function someRecursive(array, callback) {
+  array.some(isOdd);
+}
+
+console.log(someRecursive([1, 2, 3, 4], isOdd));
+console.log(someRecursive([4, 6, 8, 9], isOdd));
+console.log(someRecursive([4, 6, 8], isOdd));
