a3b7be7032f11badcc1b58cbcec79a28896026a8
Wechuli <wechulipaul@yahoo.com>
2020-01-13 11:16:27 +0300

    merge sort function

---
+++ algorithms-step-by-step/sorting-algorithms/code/playground/mergeSortFunction.js
@@ -0,0 +1,63 @@
+/*
+- create an empty array, take a look at the smallest values in each input array
+- while there are still values we haven't looked at
+    - if the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
+    - if the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
+    - once we exhaust one array, push in all remaining values from the other array
+
+*/
+
+input1 = [
+  34,
+  67,
+  78,
+  90,
+  97,
+  100,
+  101,
+  102,
+  103,
+  104,
+  105,
+  107,
+  107,
+  108,
+  200,
+  201
+];
+input2 = [-34, 0, 5, 12, 89, 98, 1002, 4564, 5000];
+
+function mergeSortFunction(array1, array2) {
+  let results = [];
+  let i = 0;
+  let j = 0;
+  while (i <= array1.length && j <= array2.length) {
+    //If both arrays have reached the end of the loop, break out of the loop
+    if (i === array1.length && j === array2.length) {
+      break;
+    }
+    // If we have reached the end of array1 and j has more elements still, copy the remaining elements of array2 into the results and then break
+    if (i === array1.length && j >= i) {
+      results.push(...array2.slice(j));
+      break;
+    }
+    // If we have reached the end of array2 and i has more elements still, copy the remaining elements of array1 into the results and then break
+    if (j === array2.length && i >= j) {
+      results.push(...array1.slice(i));
+      break;
+    }
+
+    if (array1[i] <= array2[j]) {
+      results.push(array1[i]);
+      i++;
+    } else {
+      results.push(array2[j]);
+      j++;
+    }
+    console.log(i, j);
+  }
+  return results;
+}
+
+console.log(mergeSortFunction(input1, input2));
+console.log(mergeSortFunction([1, 2, 1000, 8569], [0, 3, 4, 89]));
--- algorithms-step-by-step/sorting-algorithms/notes.md
+++ algorithms-step-by-step/sorting-algorithms/notes.md
@@ -26,3 +26,6 @@ Builds up the sort by gradually creating larger left half which is always sorted
 
 ## Merge Sort
 
+- It's a combination of two things - merging and sorting
+- Exploits the fact that arrays of 0 or 1 element are always sorted
+- Works by decomposing an array into smaller arrays of smaller arrays of 0 or 1 elements, then building up a newly sorted array
